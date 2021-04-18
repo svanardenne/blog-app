3bbf32a121665d782e6f9fae3a2f30c5650be09b
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-18 00:23:51 -0700

    Added server

--- blog-app-back/app.js
+++ blog-app-back/app.js
@@ -5,3 +5,25 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const { body } = require("express-validator");
 require("dotenv").config();
+
+// set up React app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.MONGO_DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+// set up server
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
