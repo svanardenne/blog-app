cfbf584d9e9bb8cfd32ef04a686c15bd19256a44
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-18 13:26:17 -0700

    Removed user ability to set isAdmin

--- blog-app-back/controllers/auth.js
+++ blog-app-back/controllers/auth.js
@@ -2,12 +2,17 @@ const User = require("../models/user");
 
 exports.signup = (req, res) => {
   const user = new User(req.body);
-  user.save((err, user) => {
-    if (err) {
-      return res.status(400).json({
-        error: err,
-      });
-    }
-    res.json({ user });
+  if (!req.body.isAdmin) {
+    user.save((err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: err,
+        });
+      }
+      res.json({ user });
+    });
+  }
+  res.status(401).json({
+    error: "You do not have permission to set admin",
   });
 };
