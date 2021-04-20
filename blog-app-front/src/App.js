9395ec5c0e1ef9b14b09588d449cdb3c47fb9861
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-20 11:37:19 -0700

    Added styled-normalize.

--- blog-app-front/src/App.js
+++ blog-app-front/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Routes from "./Routes";
+import { Normalize } from "styled-normalize";
 
 // Font Awesome imports
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -9,6 +10,11 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 // create Font Awesome library
 library.add(fab, fas);
 
-const App = () => <Routes />;
+const App = () => (
+  <React.Fragment>
+    <Normalize />
+    <Routes />
+  </React.Fragment>
+);
 
 export default App;
