1c360ecef2bf8496b87dd3df341419600c780d20
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-04-20 14:46:34 -0700

    Added styled components to signup form

--- blog-app-front/src/user/Signup.js
+++ blog-app-front/src/user/Signup.js
@@ -1,5 +1,17 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import Layout from "../core/Layout";
+
+// styled components
+const Form = styled.form`
+  width: 90%;
+  margin: 50px auto 50px;
+`;
+const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`;
 
 const Signup = () => {
   const [values, setValues] = useState({
@@ -24,26 +36,25 @@ const Signup = () => {
 
   return (
     <Layout title="Signup" description="Signup to the Blog App">
-      <div>
-        <form>
-          <div>
-            <label>Name</label>
-            <input onChange={handleChange("name")} type="text" value={name} />
-          </div>
-          <div>
-            <label>Email</label>
-            <input onChange={handleChange("email")} type="text" value={email} />
-          </div>
-          <div>
-            <label>Password</label>
-            <input
-              onChange={handleChange("password")}
-              type="text"
-              value={password}
-            />
-          </div>
-        </form>
-      </div>
+      <Form>
+        <FormGroup>
+          <label>Name</label>
+          <input onChange={handleChange("name")} type="text" value={name} />
+        </FormGroup>
+        <FormGroup>
+          <label>Email</label>
+          <input onChange={handleChange("email")} type="text" value={email} />
+        </FormGroup>
+        <FormGroup>
+          <label>Password</label>
+          <input
+            onChange={handleChange("password")}
+            type="text"
+            value={password}
+          />
+        </FormGroup>
+        <button type="submit">Submit</button>
+      </Form>
     </Layout>
   );
 };
