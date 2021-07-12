9f33a1e499dc8cf7816af75ad6f3721e34ae0347
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-07-12 15:06:18 -0700

    added link back to posts route from post/:slug route

--- blog-app-front/src/core/Post.js
+++ blog-app-front/src/core/Post.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { getPosts, postBySlug } from "./apiCore";
 import Layout from "./Layout";
@@ -17,7 +18,12 @@ const Post = (props) => {
     getPostBySlug(slug);
   }, []);
 
-  return <div>{JSON.stringify(post)}</div>;
+  return (
+    <div>
+      <Link to={`/posts`}>{`< Back`}</Link>
+      <div>{JSON.stringify(post)}</div>
+    </div>
+  );
 };
 
 export default Post;
