d4860391e43458aa3f8adfa8a3a0b377e0b96e64
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-07-23 15:04:17 -0700

    finished doodles page

--- blog-app-front/src/core/Doodles.js
+++ blog-app-front/src/core/Doodles.js
@@ -12,10 +12,43 @@ const Container = styled.section`
     font-size: 40px;
     font-weight: 600;
     padding: 0 24px 0 24px;
+    text-align: center;
+    line-height: 1.2;
+    margin-top: 40px;
+    margin-bottom: 40px;
+  }
+  @media ${device.tablet} {
+    h1 {
+      font-size: 48px;
+    }
+  }
+  @media ${device.laptopM} {
+    h1 {
+      font-size: 62px;
+    }
+  }
+  @media ${device.laptopL} {
+    h1 {
+      font-size: 64px;
+    }
   }
 `;
 
-const ImagesContainer = styled.div``;
+const ImagesContainer = styled.div`
+  margin: 0 auto;
+  div:last-child {
+    padding-bottom: 40px;
+  }
+  @media ${device.laptopS} {
+    max-width: 984px;
+  }
+  @media ${device.laptopM} {
+    max-width: 1160px;
+  }
+  @media ${device.laptopL} {
+    max-width: 1280px;
+  }
+`;
 
 const Doodles = () => {
   const [images, setImages] = useState([]);
--- blog-app-front/src/core/ImageItem.js
+++ blog-app-front/src/core/ImageItem.js
@@ -1,14 +1,53 @@
 import { API } from "../config";
 import styled from "styled-components";
+import { colors } from "../styles/colors";
+import { device } from "../styles/device";
+
+const Wrapper = styled.div`
+  margin: 0 24px 24px 24px;
+  p {
+    color: ${colors.offWhite};
+    margin: 0;
+    line-height: 1.5;
+    font-size: 16px;
+  }
+  @media ${device.laptopS} {
+    display: flex;
+    flex-direction: row-reverse;
+    gap: 48px;
+    p {
+      flex-basis: 30%;
+    }
+    div {
+      flex-basis: 70%;
+    }
+  }
+  @media ${device.laptopL} {
+    p {
+      font-size: 18px;
+    }
+  }
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  margin-bottom: 5px;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
 
 const ImageItem = ({ item }) => {
   return (
-    <div>
-      <div>
+    <Wrapper>
+      <ImageContainer>
         <img src={`${API}/image/${item._id}`} />
-      </div>
+      </ImageContainer>
       <p>{item.image_caption}</p>
-    </div>
+    </Wrapper>
   );
 };
 
