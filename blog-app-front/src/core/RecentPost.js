b47f3b88702b86d61009c680863dc539c8f0e243
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-07-25 01:02:41 -0700

    text overflow elipsis added on recent posts

--- blog-app-front/src/core/RecentPost.js
+++ blog-app-front/src/core/RecentPost.js
@@ -10,24 +10,34 @@ const Wrapper = styled.div`
   flex-direction: row;
   gap: 16px;
   margin-bottom: 24px;
-  p:first-child {
-    font-size: 16px;
-  }
-  p:last-child {
-    font-size: 14px;
-    color: ${colors.muted};
+  div {
+    p:first-child {
+      font-size: 16px;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+    p:last-child {
+      font-size: 14px;
+      color: ${colors.muted};
+    }
   }
 `;
 
 const RecentPost = ({ item, i }) => {
   return (
     <Wrapper>
-      <ShowBackgroundImage
-        item={item}
-        url="post"
-        width="110px"
-        height="110px"
-      />
+      <div>
+        <ShowBackgroundImage
+          item={item}
+          url="post"
+          width="110px"
+          height="110px"
+        />
+      </div>
+
       <div>
         <p>{item.title}</p>
         <p>{moment(item.created_at).format("MMM Do, YYYY")}</p>
