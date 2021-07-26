f9747d104aceaf9570b9533a0ae91d9572dfcfa7
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-07-26 10:02:06 -0700

    improved styles on blog cards on home page

--- blog-app-front/src/core/Card.js
+++ blog-app-front/src/core/Card.js
@@ -4,12 +4,13 @@ import { colors } from "../styles/colors";
 import styled from "styled-components";
 import moment from "moment";
 import ShowBackgroundImage from "./ShowBackgroundImage";
+import { device } from "../styles/device";
 
 const CardWrapper = styled.div`
   width: 245px;
   height: 330px;
   background-color: ${colors.offWhite};
-  margin-bottom: 48px;
+  margin: 0 auto 48px;
   box-shadow: 7px 7px 14px #4c5258;
   padding-top: 1px;
   img {
@@ -17,8 +18,30 @@ const CardWrapper = styled.div`
     max-width: 243px;
     margin-top: 2px;
   }
-  @media (max-width: 1024px) {
-    margin: 0 auto 48px;
+  p {
+    font-size: 14px;
+    color: ${colors.muted};
+  }
+  h4 {
+    font-size: 22px;
+    font-weight: 400;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    margin-left: 16px;
+    margin-right: 16px;
+  }
+  a {
+    text-decoration: none;
+    color: ${colors.bgBlue};
+  }
+  @media ${device.desktopS} {
+    p {
+      font-size: 16px;
+    }
+    h4 {
+      font-size: 24px;
+    }
   }
 `;
 
