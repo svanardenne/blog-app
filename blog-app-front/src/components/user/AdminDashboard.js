51f2b92491b05377b2c63f98548b3a81112f7396
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-07-28 18:51:24 -0700

    Added personalized greeting to dashboard

--- blog-app-front/src/components/user/AdminDashboard.js
+++ blog-app-front/src/components/user/AdminDashboard.js
@@ -1,11 +1,15 @@
 // main imports
 import React from "react";
 import { Link } from "react-router-dom";
+import { isAuthenticated } from "../auth";
 import Layout from "../core/Layout";
 
 const AdminDashboard = () => {
   return (
-    <Layout title="Admin Dashboard" description="Manage your app!">
+    <Layout
+      title={`Welcome, ${isAuthenticated().user.name}`}
+      description="Manage your app from here!"
+    >
       <Link to="/post/create">Create Post</Link>
       <Link to="/doodle/create">Create Doodle</Link>
     </Layout>
