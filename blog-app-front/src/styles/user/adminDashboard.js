4c280da2e482af7f9e4884fd1365dd6898d28735
Shaun van Ardenne <s.vanardenne@gmail.com>
2021-07-29 11:21:43 -0700

    finished basic styling

--- blog-app-front/src/styles/user/adminDashboard.js
+++ blog-app-front/src/styles/user/adminDashboard.js
@@ -4,4 +4,23 @@ import { device } from "../device";
 
 export const LinkList = styled.ul`
   list-style-type: none;
+  padding: 0;
+  max-width: 50%;
+  margin: 0 auto;
+  border: 1px solid ${colors.mediumGrey};
+  border-radius: 5px;
+  li {
+    position: relative;
+    border-bottom: 1px solid ${colors.mediumGrey};
+    &:last-child {
+      border: 0;
+    }
+    a {
+      display: block;
+      padding: 16px 24px;
+      text-decoration: none;
+      color: ${colors.bgBlue};
+      font-size: 22px;
+    }
+  }
 `;
